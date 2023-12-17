0df73ddbe7ddf893b9789a8f17e90e715b214baa
mohamedHaney <mohaney745@gmail.com>
2023-12-18 00:31:18 +0200

    first-commit

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css'
+const Footer = () => {
+  return (
+    <footer>
+      <span className='footer-span'><span> Copyright 2024 &copy; Built With &hearts; By </span>
+      <span>( Mohamed Haney ) </span><div className="social-links">
+        <a href="https://www.linkedin.com/in/mohamed-haney-b133b7270/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+        <a href="https://www.facebook.com/mohamed.haney.31924" target="_blank" rel="noreferrer"><i className="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/mohamed_haney.7/" target="_blank" rel="noreferrer"><i className="bi bi-instagram"></i></a>
+        <a href="https://wa.me/201273972861" target="_blank" rel="noreferrer"><i className="bi bi-whatsapp"></i></a>
+</div></span>
+    </footer>
+  );
+}
+export default Footer;
---
+++ frontend/src/utils/request.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const request = axios.create({
+    baseURL: "https://knowflow123.onrender.com"
+});
+
+export default request;
