023449a911b20aecbb2aedd702d53a935a628ac1
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2024-10-19 01:40:05 +0300

    Update HeaderLeft.jsx

--- frontend/src/components/header/HeaderLeft.jsx
+++ frontend/src/components/header/HeaderLeft.jsx
@@ -2,7 +2,7 @@ const HeaderLeft = ({ toggle, setToggle }) => {
   return (
     <div className="header-left">
       <div className="header-logo">
-        <strong>Know Flow</strong>
+        <strong>Know_Flow</strong>
         <i className="bi bi-pencil"></i>
       </div>
       <div onClick={() => setToggle((prev) => !prev)} className="header-menu">
